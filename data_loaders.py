42854fdfb941d63de3cdf8b759421ecf44bcd1ef
digesaremarco <digesaremarco@gmail.com>
2025-04-07 17:47:11 +0200

    Updated: - HANTextDataset init - MakeDataLoader

--- data_loaders.py
+++ data_loaders.py
@@ -11,7 +11,6 @@ import yaml
 from gensim.models import Word2Vec
 from datasets import load_dataset
 import torch
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 
 from gensim.models.callbacks import CallbackAny2Vec
@@ -30,24 +29,27 @@ class EpochPrinter(CallbackAny2Vec):
 
 
 class HANTextDataset:
-    def __init__(self, texts, opts):
+    def __init__(self, texts, labels, opts, vocab=None, embedding_matrix=None):
         self.texts = texts
+        self.labels = labels
         self.min_frequency = opts.word_embedding_min_frequency
         self.embedding_dim = opts.word_embedding_dim
 
         # Tokenize all texts once
+        print("Tokenizing and processing texts...")
         self.processed_texts = self.tokenize_and_process_texts()
 
-        # Build vocabulary
-        self.vocab = self.build_vocabulary()
-        # print vocabulary
-        print(self.vocab)
-
-        # Train Word2Vec
-        self.word2vec_model = self.train_word2vec()
-
-        # Build embedding matrix
-        self.embedding_matrix = self.build_embedding_matrix()
+        if vocab is None:
+            # Build vocabulary (only for train)
+            self.vocab = self.build_vocabulary()
+            # Train Word2Vec
+            self.word2vec_model = self.train_word2vec()
+            # Build embedding matrix
+            self.embedding_matrix = self.build_embedding_matrix()
+        else:
+            # Use precomputed vocab and embedding matrix for validation/test
+            self.vocab = vocab
+            self.embedding_matrix = embedding_matrix
 
     def tokenize_with_stanford(self, text):
         """Tokenizes the text using the Stanford CoreNLP server."""
@@ -66,8 +68,8 @@ class HANTextDataset:
         total_texts = len(self.texts)
 
         for idx, text in enumerate(self.texts):
-            # Print progress every 100 texts
-            if (idx + 1) % 100 == 0:
+            # Print progress every 1000 texts
+            if (idx + 1) % 1000 == 0:
                 progress = (idx + 1) / total_texts * 100
                 print(f"Progress: {idx + 1}/{total_texts} ({progress:.2f}%)")
 
@@ -123,16 +125,22 @@ class HANTextDataset:
 
     # documents of similar length (in terms of the number of sentences in the documents) are organized to be a batch. This is done to ensure that the model can be trained efficiently using mini-batch training.
     def custom_collate(batch):
-        """Collate function per organizzare i documenti in batch di lunghezza simile e applicare padding."""
+        """Collate function to organize documents into batches of similar length and pad the documents."""
+        '''print("\n=== DEBUG: Batch ricevuto da DataLoader ===")
+        print(batch)  # Stampa l'intero batch
+        print("===========================================\n")'''
+
+
+        docs, labels = zip(*batch)  # Unzip the batch
 
         # Find the maximum number of sentences and words in the batch
-        max_num_sentences = max(len(doc) for doc in batch)
+        max_num_sentences = max(len(doc) for doc in docs)
         max_num_words = max(
-            max(len(sentence) for sentence in doc) for doc in batch)  # Trova il numero massimo di parole per frase
+            max(len(sentence) for sentence in doc) for doc in docs)  # Trova il numero massimo di parole per frase
 
         # Pad the documents to have the same number of sentences and words
         padded_docs = []
-        for doc in batch:
+        for doc in docs:
             padded_sentences = []
             for sentence in doc:
                 # Padding per ogni frase (pad alle parole)
@@ -154,32 +162,62 @@ class HANTextDataset:
         # Stack the padded documents to create a batch tensor
         batch_tensor = torch.stack(padded_docs)  # (batch_size, max_sentences, max_words)
 
-        return batch_tensor
+        '''print("=== DEBUG: Output custom_collate ===")
+        print("Shape batch_tensor:", batch_tensor.shape)
+        print("Shape labels:", torch.tensor(labels).shape)
+        print("====================================")'''
+
+        return (batch_tensor, torch.tensor(labels, dtype=torch.long))
 
     def __len__(self):
         return len(self.processed_texts)
 
     def __getitem__(self, idx):
-        """Restituisce il documento con parole convertite in indici numerici"""
+        """Return the indexed document and its label."""
         indexed_doc = [
-            [self.vocab.get(word, self.vocab['UNK']) for word in sentence]
+            [min(self.vocab.get(word, self.vocab['UNK']), len(self.vocab) - 1) for word in sentence]
             for sentence in self.processed_texts[idx]
         ]
-        return indexed_doc
+        label = self.labels[idx]
+        return indexed_doc, label
 
 
 class MakeDataLoader:
-    def __init__(self, opts, dataset):
-        train, test, validation = torch.utils.data.random_split(dataset, [int(opts.train_size * len(dataset)),
-                                                                          int(opts.test_size * len(dataset)),
-                                                                          int(opts.validation_size * len(dataset))])
+    '''def __init__(self, opts, dataset):
+        #train, test, validation = torch.utils.data.random_split(dataset, [int(opts.train_size * len(dataset)),
+                                                                          #int(opts.test_size * len(dataset)),
+                                                                          #int(opts.validation_size * len(dataset))])
+        total_size = len(dataset)
+        train_size = int(opts.train_size * total_size)
+        test_size = int(opts.test_size * total_size)
+        validation_size = total_size - train_size - test_size  # Assicura che la somma sia corretta
+
+        train, test, validation = torch.utils.data.random_split(dataset, [train_size, test_size, validation_size])
 
         self.train_loader = DataLoader(train, batch_size=opts.batch_size, shuffle=True,
                                        collate_fn=HANTextDataset.custom_collate)
         self.test_loader = DataLoader(test, batch_size=opts.batch_size, shuffle=True,
                                       collate_fn=HANTextDataset.custom_collate)
         self.validation_loader = DataLoader(validation, batch_size=opts.batch_size, shuffle=True,
+                                            collate_fn=HANTextDataset.custom_collate)'''
+
+    def __init__(self, opts, train_set, validation_set):
+        self.train_loader = DataLoader(train_set, batch_size=opts.batch_size, shuffle=True,
+                                       collate_fn=HANTextDataset.custom_collate)
+        self.validation_loader = DataLoader(validation_set, batch_size=opts.batch_size, shuffle=True,
                                             collate_fn=HANTextDataset.custom_collate)
+        #self.test_loader = DataLoader(test_set, batch_size=opts.batch_size, shuffle=True,
+                                     #collate_fn=HANTextDataset.custom_collate)
+
+        test = torch.utils.data.random_split(train_set, [1500, len(train_set) - 1500])
+        self.test_loader = DataLoader(test[0], batch_size=opts.batch_size, shuffle=True,
+                                       collate_fn=HANTextDataset.custom_collate)
+
+
+
+
+
+
 
 
 """"# load Stanford Sentiment Treebank (SST) dataset
@@ -218,18 +256,16 @@ for idx, doc in enumerate(dataset.processed_texts):
 
 
 
-def main(opts):
+"""def main(opts):
     # load Stanford Sentiment Treebank (SST) dataset
     sst_dataset = load_dataset("glue", "sst2")
-    train_texts = sst_dataset['train']['sentence']
-    print(train_texts[:5])
+    sst_dataset_train = sst_dataset['train']['sentence']
+    sst_dataset_test = sst_dataset['test']['sentence']
+    sst_dataset_validation = sst_dataset['validation']['sentence']
 
-    subset_ratio = 0.1  # 10% del dataset
-    subset_size = int(len(train_texts) * subset_ratio)
+    sst_dataset = sst_dataset_train + sst_dataset_test + sst_dataset_validation
 
-    train_subset = train_texts[:subset_size]
-
-    dataset = HANTextDataset(train_subset, opts)
+    dataset = HANTextDataset(sst_dataset, opts)
     print("Processed Texts:", dataset.processed_texts)
     # print("Vocabulary:", dataset.vocab)
     print("vocab size:", len(dataset.vocab))
@@ -261,5 +297,26 @@ if __name__ == '__main__':
     opts = yaml.load(open(parser.parse_args().config), Loader=yaml.Loader)
     opts = SimpleNamespace(**opts)
     opts.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    main(opts)
+    main(opts)"""
 
+
+'''def __init__(self, texts, labels, opts):
+        self.texts = texts
+        self.labels = labels
+        self.min_frequency = opts.word_embedding_min_frequency
+        self.embedding_dim = opts.word_embedding_dim
+
+        # Tokenize all texts once
+        print("Tokenizing and processing texts...")
+        self.processed_texts = self.tokenize_and_process_texts()
+
+        # Build vocabulary
+        self.vocab = self.build_vocabulary()
+        # print vocabulary
+        #print(self.vocab)
+
+        # Train Word2Vec
+        self.word2vec_model = self.train_word2vec()
+
+        # Build embedding matrix
+        self.embedding_matrix = self.build_embedding_matrix()'''
